2c079880b40ba319916edd2e1e694eb58a44c401
jihadfathalla <jihadfathalla3@gmail.com>
2020-05-21 08:34:55 +0200

    install spatie and add roles and permissions

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\User;
+
+
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {    //Role::create(['name' => 'admin']);
+         //Role::create(['name' => 'professional']);
+         //Role::create(['name' => 'user']);
+         //Role::create(['name' => 'guest']); 
+         //Permission::create(['name' => 'admin']);
+         //Permission::create(['name' => 'professional']);
+         //Permission::create(['name' => 'user']);
+         //Permission::create(['name' => 'guest']);
+
+
+         //$role = Role::findById(4);
+         //$permission= Permission::findById(4);
+         //$role->givePermissionTo($permission);
+
+
+         //auth()->user()->assignRole('admin');
+         //return  auth()->user()->can('admin');
+         //auth()->user()->assignRole('professional');
+
+         //return  User::role('admin')->get();
+        //return  User::permission('admin')->get();
+
+
+
+
+
+
+
+
+
+        return view('home');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,8 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
 
         <!-- Styles -->
         <style>
@@ -93,6 +95,9 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
                 </div>
             </div>
         </div>
