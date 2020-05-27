27edfdea78b46f4eac81cb36680c526e9132ead1
mahmoudmoataz24 <mahmoud.moatazz24@gmail.com>
2020-05-27 18:12:49 +0200

    roles assigned to registered user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers\Auth;
 
+
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasRoles;
 class RegisterController extends Controller
 {
     /*
@@ -64,10 +66,19 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user= User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'role'=>$data['role'],
         ]);
+        $role =$data['role'];
+        if ($role=='1'){
+            $user->assignRole('user');
+        }
+        elseif ($role=='2'){
+            $user->assignRole('professional');
+        }
+        return $user;
     }
 }
