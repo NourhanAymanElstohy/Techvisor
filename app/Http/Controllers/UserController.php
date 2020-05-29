08530777e073a63cc53392aaf0c1a5f8363a7f51
nour <nour7anelsto7y@gmail.com>
2020-05-29 13:54:08 +0200

    fix usercontrroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,27 +1,25 @@
 <?php
 
 namespace App\Http\Controllers;
-<<<<<<< HEAD
-=======
-
-use Illuminate\Support\Facades\Auth;
->>>>>>> ca148419e3d6cf16f286acdb46ec2c7b6aeb0ba2
 
 use Spatie\Permission\Models\Role;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
 use App\User;
+use Illuminate\Http\Client\Request;
 
 class UserController extends Controller
 {
 
     public function index()
     {
-        $users=User::whereHas("roles", function($q){ $q->where("name", "user"); })->get();
+        $users = User::whereHas("roles", function ($q) {
+            $q->where("name", "user");
+        })->get();
         //dd($users);
-        return view('admin/user/index',[
-        'users' => $users
-            ]);
+        return view('admin/user/index', [
+            'users' => $users
+        ]);
     }
     //
     public function edit()
@@ -35,11 +33,16 @@ class UserController extends Controller
     }
     public function show(Request $request)
     {
-        $profId = $request->prof;
-        $prof = User::find($profId);
+        $userId = $request->prof;
+        $user = User::find($userId);
 
+        if (auth()->user()->hasPermissionTo('adminpermission')) {
+            return view('admin.users.show', [
+                'user' => $user
+            ]);
+        }
         return view('professionals/show', [
-            'prof' => $prof,
+            'prof' => $user,
         ]);
     }
     public function update()
@@ -50,14 +53,5 @@ class UserController extends Controller
         $user->email = $request->email;
         $user->save();
         return redirect('/');
-    }
-    public function show($id)
-    {   $request = request();
-        $userId = $request->user;
-        $user = User::find($userId);
-       
-        return view('admin.users.show', [
-             'user' => $user
-                     ]);
     }
 }
