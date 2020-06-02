1608e7d16f02ef9cb6b9fecafc46d86e61bff1f9
Heba10 <hebe@yahoo.com>
2020-06-02 14:38:51 +0200

    add bg for sign & login page on app.blade page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,114 +22,168 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link href="stylesheet" rel="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css">
     <link href="stylesheet" rel="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css">
+
+
+
+
+    <meta charset="UTF-8">
+	<title>WorkWise Html Template</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/animate.css">
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/line-awesome.css">
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/line-awesome-font-awesome.min.css">
+	<link href="{{ url('design/style') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/lib/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/lib/slick/slick-theme.css">
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/style.css">
+	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/responsive.css">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+<body class="sign-in" oncontextmenu="return false;">
+<div id="app">
 
-                    </ul>
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'Laravel') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Left Side Of Navbar -->
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
                         
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                             <span class="caret">notify</span>
+                        </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                                
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                     <span class="caret">notify</span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                              
-                                @foreach((auth()->user())->notifications as $notification)
-                                
-                                    @if($notification->type=='App\Notifications\NewQuestion')
-                                            @if($notification->unread())
-                                                    <a class="dropdown-item bg-secondary"  href="{{route('questions.show',['question'=> $notification->data['question_id']])}}">
-                                                        {{$notification->data['user_name'] }} ask {{$notification->data['question']}}
-                                                        {{$notification->markAsRead()}}
-                                                        <?php echo("</br>") ?>
-                                                    </a>
-                                            @else
-                                                    <a class="dropdown-item"  href="{{route('questions.show',['question'=> $notification->data['question_id']])}}">
-                                                            {{$notification->data['user_name'] }} ask {{$notification->data['question']}}
-                                                            {{$notification->markAsRead()}}
-                                                            <?php echo("</br>") ?>
-                                                    </a>
-
-                                            @endif  
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                      
+                        @foreach((auth()->user())->notifications as $notification)
+                        
+                            @if($notification->type=='App\Notifications\NewQuestion')
+                                    @if($notification->unread())
+                                            <a class="dropdown-item bg-secondary"  href="{{route('questions.show',['question'=> $notification->data['question_id']])}}">
+                                                {{$notification->data['user_name'] }} ask {{$notification->data['question']}}
+                                                {{$notification->markAsRead()}}
+                                                <?php echo("</br>") ?>
+                                            </a>
                                     @else
-                                            @if($notification->unread())
-                                                    <a class="dropdown-item bg-secondary"  href="#">
-                                                            {{$notification->data['user_name'] }} want to zoom meeting
-                                                            {{$notification->markAsRead()}}
-                                                            <?php echo("</br>") ?>
-                                                    </a>
-                                            @else  
-                                                     <a class="dropdown-item "  href="#">
-                                                            {{$notification->data['user_name'] }} want to zoom meeting
-                                                            {{$notification->markAsRead()}}
-                                                            <?php echo("</br>") ?>
-                                                    </a>
+                                            <a class="dropdown-item"  href="{{route('questions.show',['question'=> $notification->data['question_id']])}}">
+                                                    {{$notification->data['user_name'] }} ask {{$notification->data['question']}}
+                                                    {{$notification->markAsRead()}}
+                                                    <?php echo("</br>") ?>
+                                            </a>
 
-                                            @endif        
-                                    @endif 
-                                 
-                                   
-                                @endforeach
+                                    @endif  
+                            @else
+                                    @if($notification->unread())
+                                            <a class="dropdown-item bg-secondary"  href="#">
+                                                    {{$notification->data['user_name'] }} want to zoom meeting
+                                                    {{$notification->markAsRead()}}
+                                                    <?php echo("</br>") ?>
+                                            </a>
+                                    @else  
+                                             <a class="dropdown-item "  href="#">
+                                                    {{$notification->data['user_name'] }} want to zoom meeting
+                                                    {{$notification->markAsRead()}}
+                                                    <?php echo("</br>") ?>
+                                            </a>
+
+                                    @endif        
+                            @endif 
+                         
                            
-                                </div>
-                                
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        <main class="py-4">
-            @yield('content')
-        </main>
+                        @endforeach
+                   
+                        </div>
+                        
+                    </li>
+                @endguest
+            </ul>
+        </div>
     </div>
+</nav>
+
+
+</div>
+	
+
+	<div class="wrapper">		
+
+		<div class="sign-in-page">
+			<div class="signin-popup">
+				<div class="signin-pop">
+						
+				</div><!--signin-pop end-->
+			</div><!--signin-popup end-->
+			
+		</div><!--sign-in-page end-->
+
+
+	</div><!--theme-layout end-->
+  
     
+	<div class="footy-sec">
+				<div class="container">
+					<ul>
+						<li><a href="help-center.html" title="">Help Center</a></li>
+						<li><a href="about.html" title="">About</a></li>
+						<li><a href="#" title="">Privacy Policy</a></li>
+						<li><a href="#" title="">Community Guidelines</a></li>
+						<li><a href="#" title="">Cookies Policy</a></li>
+						<li><a href="#" title="">Career</a></li>
+						<li><a href="forum.html" title="">Forum</a></li>
+						<li><a href="#" title="">Language</a></li>
+						<li><a href="#" title="">Copyright Policy</a></li>
+					</ul>
+					<p><img src="{{ url('design/style') }}/images/copy-icon.png" alt="">Copyright 2019</p>
+				</div>
+			</div><!--footy-sec end-->
+
+
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
@@ -140,6 +194,12 @@
     $('#example').DataTable();
 } );
 </script>
+
+<script type="text/javascript" src="{{ url('design/style') }}/js/jquery.min.js"></script>
+<script type="text/javascript" src="{{ url('design/style') }}/js/popper.js"></script>
+<script type="text/javascript" src="{{ url('design/style') }}/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="{{ url('design/style') }}/lib/slick/slick.min.js"></script>
+<script type="text/javascript" src="{{ url('design/style') }}/js/script.js"></script>
 </body>
 </body>
 </html> 
