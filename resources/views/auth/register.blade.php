56d5720b715af4a87a77565821a770970d3d7e92
Heba10 <hebe@yahoo.com>
2020-06-03 21:14:47 +0200

    finish register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,10 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="login-sec">
+<div class="card-header sign_in_sec current" id="tab-2"><h3>{{ __('Register') }}</h3></div>
+
 
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
+                
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
@@ -24,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
@@ -85,7 +87,13 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="    color: #ffffff;
+    font-size: 16px;
+    background-color: #e44d3a;
+    padding: 12px 27px;
+    border: 0;
+    font-weight: 500;
+   ">
                                     {{ __('Register') }}
                                 </button>
                             </div>
@@ -93,7 +101,7 @@
                     </form>
                 </div>
             </div>
-     
+            </div><!--login-sec end-->
 {{--    <script language="JavaScript">--}}
 {{--const  role=document.getElementById('role-option');--}}
 {{--const cat =document.getElementById('cat-section');--}}
