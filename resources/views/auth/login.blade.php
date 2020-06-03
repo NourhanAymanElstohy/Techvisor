dfd823941a54619b947936dd88246fd4642a4db7
Heba10 <hebe@yahoo.com>
2020-06-03 18:35:49 +0200

    finish login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,22 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-
+<div class="login-sec">
  
-        
+<div class="card-header sign_in_sec current" id="tab-1"><h3>{{ __('Login') }}</h3></div>      
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                               
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -53,7 +53,13 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="    color: #ffffff;
+    font-size: 16px;
+    background-color: #e44d3a;
+    padding: 12px 27px;
+    border: 0;
+    font-weight: 500;
+   ">
                                     {{ __('Login') }}
                                 </button>
 
@@ -68,6 +74,6 @@
                 </div>
             </div>
        
- 
+            </div><!--login-sec end-->
 
 @endsection
