e1923e9f269fed3419dd3f957bf2738e58a5f206
nour <nour7anelsto7y@gmail.com>
2020-06-05 14:44:20 +0200

    fix register and login blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,143 +3,133 @@
 @section('content')
 
 <body class="sign-in" oncontextmenu="return false;">
-<div class="wrapper">		
-
-<div class="sign-in-page">
-    <div class="signin-popup">
-        <div class="signin-pop">
-
-
-        <div class="row">
-                <div class="col-lg-6">
-                    <div class="cmp-info">
-                        <div class="cm-logo">
-                            <img src="{{ url('design/style') }}/images/cm-logo.png" alt="">
-                            <p>Workwise,  is a global freelancing platform and social networking where businesses and independent professionals connect and collaborate remotely</p>
-                        </div><!--cm-logo end-->	
-                        <img src="{{ url('design/style') }}/images/cm-main-img.png" alt="">			
-                    </div><!--cmp-info end-->
-                </div>
-                
-
-
-
-
-
-                <div class="col-lg-6">
-                <div class="login-sec">
-
-
-
-
-
-
-
-
-<div class="login-sec">
- 
-<div class="card-header sign_in_sec current" id="tab-1"><h3>{{ __('Login') }}</h3></div>      
-<div class="main-section">
-
-
-
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                               
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+    <div class="wrapper">
+        <div class="sign-in-page">
+            <div class="signin-popup">
+                <div class="signin-pop">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="cmp-info">
+                                <div class="cm-logo">
+                                    <img src="{{ url('design/style') }}/images/cm-logo.png" alt="">
+                                    <p>Workwise, is a global freelancing platform and social networking where businesses
+                                        and independent professionals connect and collaborate remotely</p>
                                 </div>
+                                <!--cm-logo end-->
+                                <img src="{{ url('design/style') }}/images/cm-main-img.png" alt="">
+                            </div>
+                            <!--cmp-info end-->
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="login-sec">
+                                <div class="login-sec">
+
+                                    <div class="card-header sign_in_sec current" id="tab-1">
+                                        <h3>{{ __('Login') }}</h3>
+                                    </div>
+                                    <div class="main-section">
+                                        <div class="card-body">
+                                            <form method="POST" action="{{ route('login') }}">
+                                                @csrf
+
+                                                <div class="form-group row mr-5">
+                                                    <label for="email"
+                                                        class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+
+                                                    <div class="col-md-8">
+                                                        <input id="email" type="email"
+                                                            class="form-control @error('email') is-invalid @enderror"
+                                                            name="email" value="{{ old('email') }}" required
+                                                            autocomplete="email" autofocus>
+
+                                                        @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group row mr-5">
+                                                    <label for="password"
+                                                        class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                                    <div class="col-md-8">
+                                                        <input id="password" type="password"
+                                                            class="form-control @error('password') is-invalid @enderror"
+                                                            name="password" required autocomplete="current-password">
+
+                                                        @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group row">
+                                                    <div class="col-md-6 offset-md-4">
+                                                        <div class="form-check">
+                                                            <input class="form-check-input" type="checkbox"
+                                                                name="remember" id="remember"
+                                                                {{ old('remember') ? 'checked' : '' }}>
+
+                                                            <label class="form-check-label" for="remember">
+                                                                {{ __('Remember Me') }}
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                                <div class="form-group row mb-0">
+                                                    <div class="col-md-8 offset-md-4">
+                                                        <button type="submit" class="btn btn-primary"
+                                                            style="color: #ffffff; font-size: 16px; background-color: #e44d3a; padding: 12px 27px; border: 0;font-weight: 500;">
+                                                            {{ __('Login') }}
+                                                        </button>
+
+                                                        @if (Route::has('password.request'))
+                                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                                            {{ __('Forgot Your Password?') }}
+                                                        </a>
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+
+                                </div>
+
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary" style="    color: #ffffff;
-    font-size: 16px;
-    background-color: #e44d3a;
-    padding: 12px 27px;
-    border: 0;
-    font-weight: 500;
-   ">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-       
-            </div><!--login-sec end-->
-
-
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
-</div>
-</div></div>
-						</div>
 
 
 
 
 
+    </div>
+    <!--signin-pop end-->
+    </div>
+    <!--signin-popup end-->
 
 
 
-
-
-
-						
-					</div>	
-
-
-
-
-						
-				</div><!--signin-pop end-->
-            </div><!--signin-popup end-->
-
-
-
-@endsection
+    @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,176 +5,148 @@
 
 
 <body class="sign-in" oncontextmenu="return false;">
-<div class="wrapper">		
+    <div class="wrapper">
 
-<div class="sign-in-page">
-    <div class="signin-popup">
-        <div class="signin-pop">
+        <div class="sign-in-page">
+            <div class="signin-popup">
+                <div class="signin-pop">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <div class="cmp-info">
+                                <div class="cm-logo">
+                                    <img src="{{ url('design/style') }}/images/cm-logo.png" alt="">
+                                    <p>Workwise, is a professional consulting web application specially in IT fields
+                                        as it provides a rich community of professionals that can solve users’ problems
+                                        or consulting them in many different IT fields</p>
+                                </div>
+                                <!--cm-logo end-->
+                                <img src="{{ url('design/style') }}/images/cm-main-img.png" alt="">
+                            </div>
+                            <!--cmp-info end-->
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="login-sec">
+                                <div class="login-sec">
+                                    <div class="card-header sign_in_sec current" id="tab-2">
+                                        <h3>{{ __('Register') }}</h3>
+                                    </div>
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <form method="POST" action="{{ route('register') }}">
+                                                @csrf
+                                                <div class="form-group row mr-5">
+                                                    <label for="name"
+                                                        class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
+                                                    <div class="col-md-8">
+                                                        <input id="name" type="text"
+                                                            class="form-control @error('name') is-invalid @enderror"
+                                                            name="name" value="{{ old('name') }}" required
+                                                            autocomplete="name" autofocus>
 
-        <div class="row">
-                <div class="col-lg-6">
-                    <div class="cmp-info">
-                        <div class="cm-logo">
-                            <img src="{{ url('design/style') }}/images/cm-logo.png" alt="">
-                            <p>Workwise,  is a global freelancing platform and social networking where businesses and independent professionals connect and collaborate remotely</p>
-                        </div><!--cm-logo end-->	
-                        <img src="{{ url('design/style') }}/images/cm-main-img.png" alt="">			
-                    </div><!--cmp-info end-->
-                </div>
-                
+                                                        @error('name')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row mr-5">
+                                                    <label for="email"
+                                                        class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
+                                                    <div class="col-md-8">
+                                                        <input id="email" type="email"
+                                                            class="form-control @error('email') is-invalid @enderror"
+                                                            name="email" value="{{ old('email') }}" required
+                                                            autocomplete="email">
 
+                                                        @error('email')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row mr-5">
+                                                    <label for="password"
+                                                        class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
+                                                    <div class="col-md-8">
+                                                        <input id="password" type="password"
+                                                            class="form-control @error('password') is-invalid @enderror"
+                                                            name="password" required autocomplete="new-password">
 
+                                                        @error('password')
+                                                        <span class="invalid-feedback" role="alert">
+                                                            <strong>{{ $message }}</strong>
+                                                        </span>
+                                                        @enderror
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row mr-5">
+                                                    <label for="password-confirm"
+                                                        class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
+                                                    <div class="col-md-8">
+                                                        <input id="password-confirm" type="password"
+                                                            class="form-control" name="password_confirmation" required
+                                                            autocomplete="new-password">
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row mr-5">
+                                                    <label for="password-confirm"
+                                                        class="col-md-4 col-form-label text-md-right">{{ __('Register As') }}</label>
 
-                <div class="col-lg-6">
-                <div class="login-sec">
+                                                    <div class="col-md-8">
+                                                        <select class="custom-select" name="role" id="role-option">
+                                                            <option value="1">User</option>
+                                                            <option value="2">Professional</option>
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row mr-5" id="cat-section">
+                                                    <label for="password-confirm"
+                                                        class="col-md-4 col-form-label text-md-right">{{ __('Categories') }}</label>
 
-
-
-<div class="login-sec">
-<div class="card-header sign_in_sec current" id="tab-2"><h3>{{ __('Register') }}</h3></div>
-
-
-            
-
-            <div class="card">
-                
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                                    <div class="col-md-8">
+                                                        <select class="custom-select" name="categories[]" multiple>
+                                                            @foreach($categories as $cat)
+                                                            <option value="{{$cat->id}}">{{$cat->name}}</option>
+                                                            @endforeach
+                                                        </select>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group row mb-0">
+                                                    <div class="col-md-6 offset-md-4">
+                                                        <button type="submit" class="btn btn-primary"
+                                                            style="color: #ffffff;font-size: 16px;background-color: #e44d3a;padding: 12px 27px;border: 0;font-weight: 500;">
+                                                            {{ __('Register') }}
+                                                        </button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!--login-sec end-->
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Register As') }}</label>
-
-                            <div class="col-md-6">
-                                <select class="custom-select" name="role" id="role-option" >
-                                    <option value="1">User</option>
-                                    <option value="2">Professional</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="form-group row" id="cat-section">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Categories') }}</label>
-
-                            <div class="col-md-6">
-                                <select class="custom-select" name="categories[]" multiple>
-                                    @foreach($categories as $cat)
-                                        <option value="{{$cat->id}}">{{$cat->name}}</option>
-                                     @endforeach
-                                </select>
-                            </div>
-
-
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary" style="    color: #ffffff;
-    font-size: 16px;
-    background-color: #e44d3a;
-    padding: 12px 27px;
-    border: 0;
-    font-weight: 500;
-   ">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
-            </div><!--login-sec end-->
         </div>
     </div>
-</div>
-</div>
-
-
-
-
-
-</div>
-						</div>
-
-
-
-
-
-
-
-
-
-
-
-						
-					</div>	
-
-
-
-
-						
-				</div><!--signin-pop end-->
-            </div><!--signin-popup end-->
-{{--    <script language="JavaScript">--}}
-{{--const  role=document.getElementById('role-option');--}}
-{{--const cat =document.getElementById('cat-section');--}}
-{{--if(role.value=='1'){--}}
-{{--    cat.style.visibility='hidden';--}}
-{{--}--}}
-{{--else if (role.value=='2'){--}}
-{{--    cat.style.visibility='visible';--}}
-{{--}--}}
-{{--    </script>--}}
-@endsection
+    </div>
+    </div>
+    <!--signin-popup end-->
+    {{--    <script language="JavaScript">--}}
+    {{--const  role=document.getElementById('role-option');--}}
+    {{--const cat =document.getElementById('cat-section');--}}
+    {{--if(role.value=='1'){--}}
+    {{--    cat.style.visibility='hidden';--}}
+    {{--}--}}
+    {{--else if (role.value=='2'){--}}
+    {{--    cat.style.visibility='visible';--}}
+    {{--}--}}
+    {{--    </script>--}}
+    @endsection
