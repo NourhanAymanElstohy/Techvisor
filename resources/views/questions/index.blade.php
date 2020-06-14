dd5124f3ed36097eda315ed782a46245414444db
nour <nour7anelsto7y@gmail.com>
2020-06-14 15:27:15 +0200

    question index styles

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -1,24 +1,25 @@
 
 
 @foreach($questions as $question)
-<div class="post-bar">
-    <div class="post_topbar">
-        <div class="usy-dt">
-            <img src="/uploads/avatars/{{$question->user->avatar}}" width="30" height="30" alt="">
-            <div class="usy-name">
-                <h3>{{ $question->user ? $question->user->name : 'not exist'}}</h3>
-                <span><img src="workwise-new/images/clock.png" alt="">{{$question->created_at}}</span>
+<div class="posty">
+    <div class="post-bar">
+        <div class="post_topbar">
+            <div class="usy-dt">
+                <img src="/uploads/avatars/{{$question->user->avatar}}" width="30" height="30" alt="">
+                <div class="usy-name">
+                    <h3>{{ $question->user ? $question->user->name : 'not exist'}}</h3>
+                    <span><img src="workwise-new/images/clock.png" alt="">{{$question->created_at}}</span>
+                </div>
             </div>
+            @if($question->user_id==auth()->id())
+            <div class="ed-opts">
+                <a href="#" title="" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>
+                <ul class="ed-options">
+                    <li><a href="questions/{{$question->id}}/edit" title="">Edit Post</a></li>
+                </ul>
+            </div>
+            @endif
         </div>
-        @if($question->user_id==auth()->id())
-        <div class="ed-opts">
-            <a href="#" title="" class="ed-opts-open"><i class="la la-ellipsis-v"></i></a>
-            <ul class="ed-options">
-                <li><a href="questions/{{$question->id}}/edit" title="">Edit Post</a></li>
-            </ul>
-        </div>
-        @endif
-
         <div class="epi-sec">
             <ul class="descp">
                 <li><img src="workwise-new/images/icon8.png" alt=""><span>
@@ -29,7 +30,7 @@
         <div class="job_descp">
             <p>{{$question->question}}</p>
         </div>
-        
+            
         <div class="job-status-bar">
             @if($question->answers)
                 <?php $count=0 ; ?>
@@ -42,7 +43,7 @@
                 <li><a href="{{route('questions.show',['question'=> $question->id])}}" class="com"><i class="fas fa-comment-alt"></i> Comment <?php echo $count ?></a></li>
             </ul>
         </div>
-    <!--post-bar end-->
+        <!--post-bar end-->
         @if($question->answers ) 
             @foreach($question->answers as $answer)
                 @if($loop->last)
@@ -71,23 +72,22 @@
                 @endif
             @endforeach
         @endif
-        <div class="comment-section">                   
-            <div class="post-comment">
-                <div class="usy-dt">
-                
-                    <img src="/uploads/avatars/{{auth()->user() ? auth()->user()->avatar : ''}}"  width="35" height="35" alt="">
-                </div>
-                <div class="comment_box">
-                    <form  method="POST" action="{{route('answers.store')}}">
+                            
+        <div class="post-comment mt-3">
+            <div class="usy-dt mr-2">
+            
+                <img src="/uploads/avatars/{{auth()->user() ? auth()->user()->avatar : ''}}"  width="35" height="35" alt="">
+            </div>
+            <div class="comment_box">
+                <form  method="POST" action="{{route('answers.store')}}" class="d-inline">
                     @csrf
-                    <input id="q-input" type="hidden" class="form-control" name="question_id" value="{{$question->id}}">
-                        <input type="text" placeholder="Post a comment" name="answer" required>
-                        <button type="submit">Comment</button>
-                    </form>
-                </div>
+                    <input id="q-input" type="hidden" class="form-control" name="question_id" value="{{$question->id}}" width="10px">
+                    <input type="text" placeholder="Post a comment" name="answer" required>
+                    <button type="submit" class="m-0">Comment</button>
+                </form>
             </div>
         </div>
-    </div>  
+    </div>
 </div>
 @endforeach
 
@@ -138,4 +138,4 @@ const postDate=async ()=> {
 }
     createBtn.addEventListener('click', postDate);
 
-</script> --}} --}}
+</script> --}} 
