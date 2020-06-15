526c491700ff37f8d70e30c3bbfdfafab03a6708
nour <nour7anelsto7y@gmail.com>
2020-06-15 23:06:29 +0200

    fix about style

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,11 +5,11 @@
         <img src="{{ url('design/style') }}/images/about.png" alt="image">
     </div>
     <div class="bennertext">
-        <div class="innertitle">    
-            <a href="/home"><img src="{{ url('design/style') }}/images/logo2.jpeg"  width="300" height="80"alt="" class="ml-4"></a><h2 style="display:inline;">is a Consulting for IT Issues and programming<br>
-                consults Made by ITI Students.</h2>
-            <p>We connect users with professionals into one platform through asking 
-                questions or <br>Zoom meetings.</p>
+        <div class="innertitle">   
+            <a href="/home"><h1 class="font-weight-bold text-capitalize text-light" style="font-family: 'Serif', cursive; font-size:40px; display:inline;">Techvisor </h1></a><h2 style="display:inline;">is a Consulting for IT Issues and programming<br>
+                consults Made by ITI Students.</h2>	
+            <p>We connect users with professionals into one platform through asking
+                questions or <br>Zoom meetings.</p>	
         </div>
     </div>
     <span class="banner-title">About us</span>
