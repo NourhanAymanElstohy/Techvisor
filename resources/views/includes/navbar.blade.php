ef3721301f07e20d54eaf08ce3cce0f505acbc7e
nouranebrahim <nouranebrahim9@gmail.com>
2020-06-15 20:43:33 +0200

    link notification with page of create rate

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -139,14 +139,14 @@
 										@elseif($notification->type=='App\Notifications\NewRate')
 												@if($notification->unread())
 
-													<h3><a  class="dropdown-item " style="background-color:#ff6e6b;"  href="#">
+													<h3><a  class="dropdown-item " style="background-color:#ff6e6b;"  href="{{route('rate.create',['id'=> $notification->data['prof_id']])}}">
 													<p > you made zoom with {{Illuminate\Support\Str::limit($notification->data['prof_name'],1 )}} </p
 
 													{{$notification->markAsRead()}}
 													<?php echo("</br>") ?>
 													</a></h3>
 												@else
-									 				<h3><a class="dropdown-item" href="#">
+									 				<h3><a class="dropdown-item" href="{{route('rate.create',['id'=> $notification->data['prof_id']])}}">
 													 <p >you have made zoom with {{Illuminate\Support\Str::limit($notification->data['prof_name'],1)}} </p
 
 													{{$notification->markAsRead()}}
