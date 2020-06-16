7e0490e17de6db87c72abeeca109d53fd58d597a
nour <nour7anelsto7y@gmail.com>
2020-06-16 14:20:34 +0200

    display validation error

--- resources/views/admin/questions/edit.blade.php
+++ resources/views/admin/questions/edit.blade.php
@@ -53,7 +53,15 @@
         </div>
 
         <button type="submit" class="btn btn-success">Submit</button>
-        
+          @if ($errors->any())
+              <div class="alert alert-danger">
+                  <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                      @endforeach
+                  </ul>
+              </div>
+            @endif
       </form>  
 </div>  </div>
 </div>
