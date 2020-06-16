51a81e7fa93529de101d4f00747f5e81e59d655e
jihadfathalla <jihadfathalla3@gmail.com>
2020-06-16 15:33:50 +0200

    sidebar admin

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -11,7 +11,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{ asset('design/AdminLTE/dist/img/avatar5.png') }}" class="img-circle elevation-2" alt="User Image">
+          <img src="/uploads/avatars/{{ Auth::user()->avatar }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <h5 style="color:#3cb371"><strong>Admin</strong></h5>
