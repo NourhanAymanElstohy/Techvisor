10ffdece62b2ee488d06fd35e31b6977ec893add
nour <nour7anelsto7y@gmail.com>
2020-06-17 04:55:52 +0200

    fix header bugs

--- resources/views/includes/header2.blade.php
+++ resources/views/includes/header2.blade.php
@@ -12,17 +12,17 @@
 	
 	<link href="{{ asset('design/style/images/t-icon.png') }}" rel="shortcut icon" type="image/png">
 
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/animate.css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/line-awesome.css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/line-awesome-font-awesome.min.css">
-	<link href="{{ url('design/style') }}/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/font-awesome.min.css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/jquery.mCustomScrollbar.min.css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/lib/slick/slick.css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/lib/slick/slick-theme.css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/style.css">
-	<link rel="stylesheet" type="text/css" href="{{ url('design/style') }}/css/responsive.css">
+	<link rel="stylesheet" type="text/css" href="/design/style/css/animate.css">
+	<link rel="stylesheet" type="text/css" href="/design/style/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="design/style/css/line-awesome.css">
+	<link rel="stylesheet" type="text/css" href="/design/style/css/line-awesome-font-awesome.min.css">
+	<link href="/design/style/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" type="text/css" href="/design/style/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="/design/style/css/jquery.mCustomScrollbar.min.css">
+	<link rel="stylesheet" type="text/css" href="/design/style/lib/slick/slick.css">
+	<link rel="stylesheet" type="text/css" href="/design/style/lib/slick/slick-theme.css">
+	<link rel="stylesheet" type="text/css" href="/design/style/css/style.css">
+	<link rel="stylesheet" type="text/css" href="/design/style/css/responsive.css">
 	<link href="https://fonts.googleapis.com/css2?family=Chewy&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Gochi+Hand&display=swap" rel="stylesheet">
 
